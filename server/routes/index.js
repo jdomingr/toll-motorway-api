6d3be8ba174cd401cd3ece260c28dd5d2b56f95b
jdomingr <juandominguezriquelme@gmail.com>
2020-11-12 22:53:20 -0300

    import new routes in routes/index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,5 +6,9 @@ app.use(require('./login'));
 app.use(require('./user'));
 app.use(require('./city'));
 app.use(require('./state'));
+app.use(require('./payLog'));
+app.use(require('./tollBooth'));
+app.use(require('./tollRoad'));
+app.use(require('./vehicle'));
 
 module.exports = app;
